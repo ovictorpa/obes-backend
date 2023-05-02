7724554d8bd272686ded2241f14860b3a7d771c7
Victor Anthony <anthonyvictor90@gmail.com>
2023-05-01 22:37:29 -0300

    refactoring sql files

--- src/app/sql/insert_users.sql
+++ src/app/sql/insert_users.sql
@@ -1,3 +1,3 @@
 INSERT INTO OBES.USER (NAME, EMAIL, PASSWORD, INSTITUTION_TYPE)
-VALUES ('Victor Anthony', 'email@domain.com', 'password01'),
+VALUES ('Victor Anthony', 'email@domain.com', 'password01', null),
        ('EEMTI Escola do Amor', 'email@domain.com', 'password01', 'ESCOLA');
