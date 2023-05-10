504d543e714acb062365c2e17f1ada0e33bf628a
Herik Cosmo <herikcosmo.m@gmail.com>
2023-05-10 09:32:31 -0300

    feat: add cors and helmet

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 const path = require('path')
 const express = require("express");
-const app = express();
-const door = 3000;
+const helmet = require('helmet')
+const cors = require('cors')
 
 const bookRoutes = require('../src/routes/booksRoutes')
 const userRoute = require('../src/routes/usersRoutes')
 const loginRoutes = require('../src/routes/loginRoutes')
 const apiRoute = '/api';
 
+const app = express();
+const door = 3000;
+
+const whitelist = [
+  'http://localhost:8080',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions))
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
