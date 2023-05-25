f5e9c41a0cd60b4703d370921d5eeea130fe7df6
Victor Anthony <anthonyvictor90@gmail.com>
2023-05-24 22:21:55 -0300

    adding swagger.json

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const app = require('./app');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./../swagger.json')
 
 const port = 3000;
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(port, () => console.log(`📚 Obes API started at http://localhost:${port}`));
+  app.listen(port, () => console.log(`📚 Obes API started at http://localhost:${port}/api-docs`));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
