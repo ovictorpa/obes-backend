4a595f9d9d34551ffd9f5cbffe3130d5b05e31fd
Herik Cosmo <herikcosmo.m@gmail.com>
2023-05-29 14:23:30 -0300

    feat: more restrictions on donation orders

--- src/app/services/DonationOrdersService.js
+++ src/app/services/DonationOrdersService.js
@@ -1,7 +1,9 @@
+const UserType = require('../models/enums/UserType');
 const BooksRepository = require('../repositories/BooksRepository');
 const DonationOrdersRepository = require('../repositories/DonationOrdersRepository');
 const BadRequest = require('./errors/BadRequest');
 const NotFound = require('./errors/NotFound');
+const UsersService = require('./UsersService');
 
 class DonationOrdersService {
   constructor() {
@@ -10,10 +12,22 @@ class DonationOrdersService {
   }
 
   async createOrder({ book_id, user_id}) {
+    const usersService = new UsersService();
+
+    const user = await usersService.findById(user_id);
+
+    if(user.user_type !== UserType.Institutional) {
+      throw new BadRequest('User Is Not Institutional Type');
+    }
+
     const book = await this.booksRepository.findById(book_id);
 
     if (!book) {
       throw new NotFound('Book Not Found');
+    }
+
+    if(book.user_id === user_id) {
+      throw new BadRequest('Book Cannot Be Requested by the Same User Who Registered');
     }
 
     if(!book.available) {
